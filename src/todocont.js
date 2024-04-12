b6593e395a632dcdc87a274f1eeaec6abc6b1357
Ranjit k <k.ranjitkumar1479@gmail.com>
2024-04-12 22:17:10 +0530

    completed funtionality

--- src/todocont.js
+++ src/todocont.js
@@ -5,12 +5,10 @@ import { Strike } from "./strike"
 export function Todocontainer(){
   const {todolist,deletetodo,updatetodo}=useContext(todocontext)
   let [changekey,setchangekey]=useState(0);
-  let upnewtodo='';
+  let [upnewtodo,setnewtodo]=useState('');
   const inputtab=(event,key)=>{
-    if(event.key==="Enter"){
       updatetodo(event.target.value,key)
       setchangekey(0)
-    }
   }
   return (
     <div id="todolist">{
@@ -18,8 +16,8 @@ export function Todocontainer(){
         key++
         return(
          <div>
-          <input type ="checkbox" id={"checkbox"+(key)} checked={todoval.marked} onClick={()=>updatetodo(false,key,!todoval.marked)}/>
-          { changekey===key ? <input type="text" value={todoval.todoname} onKeyDown={(event)=>inputtab(event,key)}/>:
+          <input type ="checkbox" id={"checkbox"+(key)} checked={todoval.marked} onChange={()=>updatetodo(false,key,!todoval.marked)}/>
+          { changekey===key ? <input type="text" defaultValue={todoval.todoname} onKeyDown={(event)=>event.key=="Enter" && inputtab(event,key)}/>:
             <Strike todoval={todoval}/>
           }
           <button id={"edit"+(key)} onClick={()=>setchangekey(key)}>edit</button>
